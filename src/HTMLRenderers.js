a053d7924fe0d5cb0d38993ed10b559ff5b755bc
Антон <flatron_vi@mail.ru>
2019-10-10 17:07:27 +0300

    Update HTMLRenderers.js

--- src/HTMLRenderers.js
+++ src/HTMLRenderers.js
@@ -99,7 +99,7 @@ export function ul (htmlAttribs, children, convertedCSSStyles, passProps = {}) {
             }
         }
         return (
-            <View key={`list-${nodeIndex}-${index}-${key}`} style={{ flexDirection: 'row', marginBottom: 10 }}>
+            <View key={`list-${nodeIndex}-${index}-${key}`} style={{ flexDirection: 'row' }}>
                 { prefix }
                 <View style={{ flex: 1 }}>{ child }</View>
             </View>
